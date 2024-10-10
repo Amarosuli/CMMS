a752daa1bcdb89ae41670c89d51d3d7e0a0dcbd7
amarosuli <vuvlnvw@gmail.com>
2024-10-10 19:22:36 +0700

    [add] hooks.server.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,15 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import type { TypedPocketBase } from '$lib/CostumTypes';
+import { type AuthModel } from 'pocketbase';
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			pb: TypedPocketBase;
+			user: AuthModel | undefined;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
