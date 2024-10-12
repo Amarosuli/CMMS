298fb5e2a1ea1aefa43755d141b2d83532254840
amarosuli <vuvlnvw@gmail.com>
2024-10-12 23:31:47 +0700

    [rename]

--- src/lib/layouts/pages/index.ts
+++ src/lib/layouts/pages/index.ts
@@ -1,5 +1,5 @@
-export { default as MaterialMasterPreview } from './MaterialMasterPreview.svelte';
-export { default as MaterialUnitPreview } from './MaterialUnitPreview.svelte';
+export { default as MaterialMaster } from './MaterialMaster.svelte';
+export { default as MaterialUnit } from './MaterialUnit.svelte';
 export { default as TransactionType } from './TransactionType.svelte';
 export { default as StockAvailable } from './StockAvailable.svelte';
 export { default as LoginDialog } from './LoginDialog.svelte';
