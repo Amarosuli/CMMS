3a4118fbd0e24e82c3b4ca3a431f5b8870e6e1cd
amarosuli <vuvlnvw@gmail.com>
2024-10-12 23:28:01 +0700

    [add] more interface

--- src/lib/CostumTypes.ts
+++ src/lib/CostumTypes.ts
@@ -8,14 +8,19 @@ export interface MaterialMaster extends RecordModel {
 	part_number_3: string;
 	minimum_quantity: string;
 	remark: string;
-	expand: {
+	unit_id: MaterialUnit['id'];
+	expand?: {
 		unit_id: MaterialUnit;
 	};
 	unitCode: string;
-	unit_id: MaterialUnit['id'];
 }
 
 export interface MaterialUnit extends RecordModel {
+	code: string;
+	description: string;
+}
+
+export interface TransactionType extends RecordModel {
 	code: string;
 	description: string;
 }
@@ -29,11 +34,37 @@ export interface User extends RecordModel {
 	unit: string;
 }
 
+export interface StockMaster extends RecordModel {
+	material_id: MaterialMaster['id'];
+	quantity_available: number;
+	batch_number: string;
+	expand?: {
+		material_id: MaterialMaster;
+	};
+}
+
+export interface StockMovement extends RecordModel {
+	material_id: MaterialMaster['id'];
+	transaction_type_id: TransactionType['id'];
+	order_number: string;
+	engine_serial_number: string;
+	batch_number: string;
+	quantity: number;
+	user_id: User['id'];
+	remark: string;
+	expand?: {
+		material_id: MaterialMaster;
+		transaction_type_id: TransactionType;
+		user_id: User;
+	};
+}
+
 export interface TypedPocketBase extends PocketBase {
 	collection(idOrName: string): RecordService;
 	collection(idOrName: 'users'): RecordService<User>;
 	collection(idOrName: 'material_unit'): RecordService<MaterialUnit>;
 	collection(idOrName: 'material_master'): RecordService<MaterialMaster>;
+	collection(idOrName: 'stock_master'): RecordService<StockMaster>;
 }
 
 export type FileUrlOption = {
