a684cdf67a3862fe42286d3cf1ea3e601362efe8
amarosuli <vuvlnvw@gmail.com>
2024-10-12 14:56:45 +0700

    [add] LoginDialog

---
+++ src/lib/layouts/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as MaterialMasterPreview } from './MaterialMasterPreview.svelte';
+export { default as MaterialUnitPreview } from './MaterialUnitPreview.svelte';
+export { default as LoginDialog } from './LoginDialog.svelte';
