adfc06191fec5d5840235f0f3e00126b516e5b8e
amarosuli <vuvlnvw@gmail.com>
2024-10-14 13:59:36 +0700

    [add] materialUnitSchema

--- src/lib/zodSchema.ts
+++ src/lib/zodSchema.ts
@@ -4,3 +4,8 @@ export const loginSchema = z.object({
 	employeeId: z.string().trim().min(6, 'Employee Id is required, Minimal 6 Characters'),
 	password: z.string().trim().min(8, 'Minimal password is 8 Characters')
 });
+
+export const materialUnitSchema = z.object({
+	code: z.string().trim().min(1, 'Unit code is required'),
+	description: z.string().trim()
+});
