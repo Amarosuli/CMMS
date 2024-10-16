f7abee9be6be5a912e96bd96677c76842f3b93ef
amarosuli <vuvlnvw@gmail.com>
2024-10-16 22:50:17 +0700

    [add] Stock Out

---
+++ src/routes/movement/stock-out/+page.server.ts
@@ -0,0 +1,86 @@
+import { fail, message, superValidate } from 'sveltekit-superforms';
+import { stockOutSchema } from '$lib/zodSchema';
+import { redirect } from '@sveltejs/kit';
+import { zod } from 'sveltekit-superforms/adapters';
+import type { RecordModel } from 'pocketbase';
+
+export const load = async ({ locals }) => {
+	if (!locals.user) throw redirect(302, '/'); // Prevent guest users from accessing this page directly.
+
+	const getStock = async () => {
+		const result = await locals.pb.collection('stock_master').getFullList({ expand: 'material_id,material_id.unit_id,stock_in_id' });
+		return result.map(({ id, batch_number, expand, quantity_available }) => {
+			return {
+				value: id,
+				quantity_available,
+				label: batch_number + ' - ' + expand?.material_id.code,
+				detail: expand?.material_id.code + ' - ' + expand?.material_id.part_number,
+				unit: expand?.material_id.expand?.unit_id.code || ''
+			};
+		});
+	};
+
+	const getTransactionType = async () => {
+		const result = await locals.pb.collection('transaction_type').getFullList();
+		return result.map(({ id, code, description }) => {
+			return { label: code, value: id, description };
+		});
+	};
+
+	return {
+		transactionType: await getTransactionType(),
+		stock: await getStock(),
+		form: await superValidate(zod(stockOutSchema))
+	};
+};
+export const actions = {
+	save: async ({ locals, request }) => {
+		const form = await superValidate(request, zod(stockOutSchema));
+
+		if (!form.valid) return fail(400, { form });
+
+		let result: RecordModel;
+		try {
+			result = await locals.pb.collection('stock_out').create(form.data);
+		} catch (er: any) {
+			const errorMessage = `${er?.response.message} | PocketBase error.`;
+			return message(form, errorMessage, { status: er?.status });
+		}
+
+		return message(form, { text: 'Create Stock Out Successfully!', result });
+	},
+	updateToStockMaster: async ({ locals, request }) => {
+		const form = await request.formData();
+		const id = form.get('stock_id')?.toString();
+		const quantity = form.get('quantity')?.toString();
+
+		if (!id) return fail(400, { message: 'No id provided' });
+
+		try {
+			const stock = await locals.pb.collection('stock_master').getOne(id);
+
+			let quantity_remain = stock.quantity_available - Number(quantity);
+			let isQuantityZero = quantity_remain === 0;
+
+			if (Math.sign(quantity_remain) === -1) {
+				return fail(401, {
+					message: 'Stock out quantity is over the available quantity.'
+				});
+			} else if (isQuantityZero) {
+				await locals.pb.collection('stock_master').update(id, {
+					quantity_available: quantity_remain,
+					status: 'INACTIVE'
+				});
+			} else {
+				await locals.pb.collection('stock_master').update(id, {
+					quantity_available: quantity_remain
+				});
+			}
+		} catch (er: any) {
+			const errorMessage = `${er?.response.message} | PocketBase error.`;
+			return fail(er?.status, { message: errorMessage });
+		}
+
+		return JSON.stringify({ message: 'Update to Stock Master Successfully' });
+	}
+};
---
+++ src/routes/movement/stock-out/+page.svelte
@@ -0,0 +1,198 @@
+<script lang="ts">
+	import * as Popover from '$lib/components/ui/popover/index.js';
+	import * as Command from '$lib/components/ui/command/index.js';
+
+	import { LoaderCircle, ChevronLeft, CalendarPlus, ChevronsUpDown, Check } from 'lucide-svelte';
+	import { FieldErrors, Control, Field, Label } from '$lib/components/ui/form';
+	import { Button, buttonVariants } from '$lib/components/ui/button';
+	import { deserialize } from '$app/forms';
+	import { superForm } from 'sveltekit-superforms';
+	import { Input } from '$lib/components/ui/input';
+	import { toast } from 'svelte-sonner';
+	import { goto } from '$app/navigation';
+	import { time } from '$lib/helpers.js';
+	import { tick } from 'svelte';
+	import { cn } from '$lib/utils.js';
+
+	export let data;
+
+	const { user, transactionType, stock } = data;
+
+	const createStockMaster = async (data: Record<string, any>) => {
+		let formData = new FormData();
+		for (let key in data) {
+			formData.append(key, data[key]);
+		}
+
+		const response = await fetch('?/updateToStockMaster', {
+			method: 'POST',
+			body: formData
+		});
+
+		return deserialize(await response.text());
+	};
+
+	const form = superForm(data.form, {
+		onSubmit({ cancel }) {
+			if (Math.sign(Number(remainingStock)) === -1) {
+				toast.error('Stock out quantity is over than available quantity.');
+				cancel();
+			}
+		},
+		onUpdate({ form, result }) {
+			if (form.valid) {
+				toast.success(form.message.text);
+
+				if (result.type === 'success') {
+					let data = form.message.result;
+					let redirectUrl = `/stock/${data.stock_id}`;
+
+					createStockMaster({
+						stock_id: data.stock_id,
+						quantity: data.quantity
+					})
+						.then((res: Record<string, any>) => {
+							toast.success(JSON.parse(res.data).message);
+						})
+						.catch((er) => {
+							toast.error(er);
+						})
+						.finally(() => {
+							goto(redirectUrl);
+						});
+				}
+
+				goto('/stock');
+			}
+		}
+	});
+	const { form: formData, delayed, message, enhance } = form;
+
+	let open = false;
+	$: $formData.user_id = user ? user.id : '';
+	$: $formData.transaction_type = transactionType.find(({ label }: { label: string }) => label == 'SOUT')?.label as string;
+
+	function closeAndFocusTrigger(triggerId: string) {
+		open = false;
+		tick().then(() => {
+			document.getElementById(triggerId)?.focus();
+		});
+	}
+
+	$: selectedStock = stock.find((f) => f.value === $formData.stock_id);
+
+	let remainingStock: string;
+	$: remainingStock = selectedStock?.quantity_available.toString() || '';
+	$: if ($formData.quantity) {
+		remainingStock = (Number(remainingStock) - $formData.quantity).toString();
+	}
+</script>
+
+<svelte:head>
+	<title>CMMS - Stock Out</title>
+</svelte:head>
+
+<div>
+	<Button href="/movement" variant="outline" class="inline-flex items-center gap-2 text-sm/6">
+		<ChevronLeft class="h-4 w-4" />
+		<span>Movement</span>
+	</Button>
+</div>
+
+<div class="mt-4 lg:mt-8">
+	<div class="flex items-center gap-4">
+		<h1 class="text-2xl/8 font-semibold sm:text-xl/8">Create <span class="text-foreground/50">Stock Out</span></h1>
+		<span class="inline-flex items-center gap-x-1.5 rounded-md bg-lime-400/20 px-1.5 py-0.5 text-sm/5 font-medium text-lime-700 group-data-[hover]:bg-lime-400/30 dark:bg-lime-400/10 dark:text-lime-300 dark:group-data-[hover]:bg-lime-400/15 sm:text-xs/5 forced-colors:outline">{$formData.transaction_type}</span>
+	</div>
+	<div class="isolate mt-2.5 flex flex-wrap justify-between gap-x-6 gap-y-4">
+		<div class="flex flex-wrap gap-x-10 gap-y-4 py-1.5">
+			<span class="flex items-center gap-3 text-base/6 sm:text-sm/6">
+				<CalendarPlus class="h-4 w-4" />
+				<span>{time(new Date())}</span></span>
+		</div>
+	</div>
+</div>
+
+<div class="mt-12">
+	<h2 class="text-base/7 font-semibold text-foreground sm:text-sm/6">Stock Out Form Field</h2>
+	<hr role="presentation" class="mt-4 w-full border-t border-foreground/10" />
+	<form class="mt-3 flex w-full max-w-80 flex-col text-base/6 sm:text-sm/6" action="?/save" method="post" use:enhance>
+		<Field {form} name="stock_id" class="flex flex-col">
+			<Popover.Root bind:open let:ids>
+				<Control let:attrs>
+					<Label>Stock {remainingStock ? `- Available Qty : ${remainingStock} ${selectedStock?.unit || ''}` : ''}</Label>
+					<Popover.Trigger class={cn(buttonVariants({ variant: 'outline' }), ' justify-between', !$formData.stock_id && 'text-muted-foreground')} role="combobox" {...attrs}>
+						{stock.find((f) => f.value === $formData.stock_id)?.detail ?? 'Select Material'}
+						<ChevronsUpDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
+					</Popover.Trigger>
+					<input hidden value={$formData.stock_id} name={attrs.name} />
+				</Control>
+				<Popover.Content class="p-0">
+					<Command.Root>
+						<Command.Input autofocus placeholder="Search Stock..." class="h-9" />
+						<Command.Empty>No stock found.</Command.Empty>
+						<Command.Group class="max-h-56 overflow-y-auto">
+							{#each stock as stock}
+								<Command.Item
+									value={stock.detail}
+									onSelect={() => {
+										$formData.stock_id = stock.value;
+										closeAndFocusTrigger(ids.trigger);
+									}}>
+									{stock.label}
+									<Check class={cn('ml-auto h-4 w-4', stock.value !== $formData.stock_id && 'text-transparent')} />
+								</Command.Item>
+							{/each}
+						</Command.Group>
+					</Command.Root>
+				</Popover.Content>
+			</Popover.Root>
+			<FieldErrors class="text-xs italic" />
+		</Field>
+		<Field {form} name="transaction_type" class="hidden">
+			<Control let:attrs>
+				<Label>Transaction Type</Label>
+				<Input {...attrs} bind:value={$formData.transaction_type} type="text" placeholder="Status" />
+			</Control>
+			<FieldErrors class="text-xs italic" />
+		</Field>
+		<Field {form} name="quantity">
+			<Control let:attrs>
+				<Label>Quantity</Label>
+				<Input {...attrs} bind:value={$formData.quantity} min="0" type="number" placeholder="Quantity" />
+			</Control>
+			<FieldErrors class="text-xs italic" />
+		</Field>
+		<Field {form} name="remark">
+			<Control let:attrs>
+				<Label>Remark</Label>
+				<Input {...attrs} bind:value={$formData.remark} type="text" placeholder="Remark" />
+			</Control>
+			<FieldErrors class="text-xs italic" />
+		</Field>
+		<Field {form} name="user_id" class="hidden">
+			<Control let:attrs>
+				<Label>User</Label>
+				<Input {...attrs} bind:value={$formData.user_id} type="text" placeholder="Material Description" />
+			</Control>
+			<FieldErrors class="text-xs italic" />
+		</Field>
+		<Field {form} name="refference_id">
+			<Control let:attrs>
+				<Label>Refference</Label>
+				<Input {...attrs} bind:value={$formData.refference_id} type="text" placeholder="Refference" />
+			</Control>
+			<FieldErrors class="text-xs italic" />
+		</Field>
+		<Button class="mt-4" type="submit" disabled={$delayed ? true : false}>
+			{#if $delayed}
+				<LoaderCircle class="mr-2 h-4 w-4 animate-spin" /> Saving...
+			{:else}
+				Save
+			{/if}
+		</Button>
+		{#if $message}
+			<p class="mt-2 bg-destructive p-2 text-center text-xs font-semibold text-destructive-foreground">{$message}</p>
+		{/if}
+	</form>
+</div>
