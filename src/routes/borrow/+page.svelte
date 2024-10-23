9218adb1f0a956f534262b7b342b8e2ef2f3271a
amarosuli <vuvlnvw@gmail.com>
2024-10-23 19:43:13 +0700

    [initial] branch commit

---
+++ src/routes/borrow/+page.svelte
@@ -0,0 +1,7 @@
+<div class="flex flex-col gap-2">
+	<p>this page only for production</p>
+	<p>just welcome and there's two buttons, borrowing, or status if there's an active borrowing</p>
+	<p>there's also a history of borrowing</p>
+	<p>borrowing button will goto /production/borrow</p>
+	<p></p>
+</div>
