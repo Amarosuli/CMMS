8f7d4f020d347ecdf1c09abc874ec7632724e97d
amarosuli <vuvlnvw@gmail.com>
2024-11-06 19:01:42 +0700

    [update] now openBorrowing have isCheckOut

--- src/routes/active-borrowing/+page.server.ts
+++ src/routes/active-borrowing/+page.server.ts
@@ -2,7 +2,7 @@ export const load = async ({ locals }) => {
 	const getOpenBorrowing = async () => {
 		const result = await locals.pb.collection('borrow_movement').getFullList({ filter: 'status="OPEN" || status="PENDING"', expand: 'user_id' });
 		return result.map((r) => {
-			return { ...r, user: r.expand?.user_id || undefined };
+			return { ...r, user: r.expand?.user_id || undefined, isCheckOut: false };
 		});
 	};
 	return {
