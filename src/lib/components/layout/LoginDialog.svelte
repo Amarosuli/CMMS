492f1ab5f947195068ce650f6d5cd2c32343ac6a
amarosuli <vuvlnvw@gmail.com>
2024-11-08 00:36:30 +0700

    [update] ux for better loginDialog

--- src/lib/components/layout/LoginDialog.svelte
+++ src/lib/components/layout/LoginDialog.svelte
@@ -5,15 +5,22 @@
 	import { loginSchema } from '$lib/zodSchema';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import { Input } from '$lib/components/ui/input';
+	import { toast } from 'svelte-sonner';
 	import { zod } from 'sveltekit-superforms/adapters';
 	// icons
 	import { LoaderCircle } from 'lucide-svelte';
 
 	export let open: boolean = false;
 
+	let errorMessage = '';
+
 	const form = superForm(defaults(zod(loginSchema)), {
 		SPA: true,
 		validators: zod(loginSchema),
+		resetForm: false,
+		onChange() {
+			errorMessage = '';
+		},
 		async onUpdate({ form }) {
 			if (form.valid) {
 				const response = await fetch('/auth', {
@@ -23,20 +30,23 @@
 						'Content-Type': 'application/json'
 					}
 				});
-				const { message } = await response.json();
-				if (message === 'success') location.reload();
-			}
-		},
-		onUpdated({ form }) {
-			if (form.valid) {
-				open = !open;
+				const { message, status } = await response.json();
+				if (status === 400) {
+					errorMessage = message;
+					toast.error('Authentication Error');
+				} else if (status === 200) location.reload();
 			}
 		}
 	});
-	const { form: formData, validateForm, delayed, message, enhance } = form;
+	const { form: formData, delayed, enhance, reset } = form;
+
+	$: if (!open) {
+		reset();
+		errorMessage = '';
+	}
 </script>
 
-<Dialog.Root bind:open>
+<Dialog.Root bind:open closeOnOutsideClick={false}>
 	<Dialog.Content class="p-10">
 		<Dialog.Header>
 			<Dialog.Title>Login</Dialog.Title>
@@ -66,8 +76,8 @@
 						Let's go!
 					{/if}
 				</Button>
-				{#if $message}
-					<p class="mt-2 bg-destructive p-2 text-center text-xs font-semibold text-destructive-foreground">{$message}</p>
+				{#if errorMessage}
+					<p class="mt-2 bg-destructive/50 p-2 text-center text-xs font-semibold text-destructive-foreground">{errorMessage}</p>
 				{/if}
 			</form>
 		</div>
