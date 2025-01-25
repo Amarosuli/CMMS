1e5ce5cb7c57d3a9b1d8cc04d9cc38c3dc51428f
amarosuli <vuvlnvw@gmail.com>
2025-01-25 19:30:15 +0700

    [update] layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,12 @@
 
 	import { Navbar, NavbarSmall, LoginDialog } from '$lib/components/layout';
 	import { afterNavigate, beforeNavigate } from '$app/navigation';
-	// import { createRender } from 'svelte-headless-table';
 	import { ModeWatcher } from 'mode-watcher';
 	import { Toaster } from '$lib/components/ui/sonner/index.js';
 	import { fade } from 'svelte/transition';
 	import { page } from '$app/state';
 	// icons
-	import { SquareKanban, SendToBack, Archive, FolderInput, FolderOutput, House, ListOrdered, LoaderCircle } from 'lucide-svelte';
+	import { SendToBack, Archive, FolderInput, FolderOutput, House, LoaderCircle, Settings } from 'lucide-svelte';
 
 	interface Props {
 		data: any;
@@ -68,12 +67,12 @@
 			sub: [
 				{
 					title: 'Stock In',
-					hash: '/stock-in',
+					url: '/stock-in',
 					role: Role.ADMIN
 				},
 				{
 					title: 'Stock Out',
-					hash: '/stock-out',
+					url: '/stock-out',
 					role: Role.ADMIN
 				}
 			]
@@ -85,28 +84,10 @@
 			role: Role.ADMIN
 		},
 		{
-			title: 'Master',
-			icon: SquareKanban,
-			url: '/manage',
-			defaultHash: '/material-master',
-			role: Role.SUPER,
-			sub: [
-				{
-					title: 'Material Master',
-					hash: '/material-master',
-					role: Role.SUPER
-				},
-				{
-					title: 'Material Unit',
-					hash: '/material-unit',
-					role: Role.SUPER
-				},
-				{
-					title: 'Transaction Type',
-					hash: '/transaction-type',
-					role: Role.SUPER
-				}
-			]
+			title: 'Config',
+			icon: Settings,
+			url: '/config',
+			role: Role.SUPER
 		}
 	];
 
@@ -123,13 +104,6 @@
 	});
 </script>
 
-{#if isLogOut || loadingPage}
-	<div transition:fade class="fixed inset-0 z-[1000] flex h-screen w-screen items-center justify-center bg-secondary/80">
-		<LoaderCircle class="h-4 w-4 animate-spin text-primary" />
-		<p class="ml-2 text-xs">Loading...</p>
-	</div>
-{/if}
-
 <Toaster />
 <ModeWatcher />
 <div class="relative isolate flex min-h-svh w-full bg-secondary-foreground/5 max-lg:flex-col">
@@ -137,10 +111,16 @@
 	<NavbarSmall bind:currentHash bind:currentPath {sidebarMenu} {user} {currentRole} bind:openLoginDialog {logOut} />
 	<main class="flex flex-1 flex-col pb-2 lg:min-w-0 lg:pl-64 lg:pr-2 lg:pt-2">
 		<LoginDialog bind:open={openLoginDialog} />
-		<div class="grow bg-background p-6 text-foreground lg:rounded-lg lg:p-10 lg:shadow-sm lg:ring-1 lg:ring-secondary-foreground/10">
+		<div class="relative grow overflow-hidden bg-background p-6 text-foreground lg:rounded-lg lg:p-10 lg:shadow-sm lg:ring-1 lg:ring-secondary-foreground/10">
 			<div class="mx-auto max-w-6xl">
 				{@render children?.()}
 			</div>
+			{#if isLogOut || loadingPage}
+				<div transition:fade={{ duration: 400 }} class="absolute inset-0 z-10 flex h-screen w-full items-center justify-center bg-foreground/10">
+					<LoaderCircle class="h-4 w-4 animate-spin text-primary" />
+					<p class="ml-2 text-xs">Loading...</p>
+				</div>
+			{/if}
 		</div>
 	</main>
 </div>
