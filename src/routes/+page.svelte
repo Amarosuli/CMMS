b119cf97e8af6d43c0fef5328c072c7d73628f03
amarosuli <vuvlnvw@gmail.com>
2025-02-19 11:19:47 +0700

    [update] dashboard now have the real value

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,9 @@
 	import { RecentTransaction } from '$lib/components/page';
 	import { Button } from '$lib/components/ui/button';
 	import { time, timeOfDay } from '$lib/helpers';
+	import { pb } from '$lib/pocketbaseClient';
+	import { onMount } from 'svelte';
+	import NumberFlow from '@number-flow/svelte';
 
 	interface Props {
 		data: any;
@@ -20,48 +23,217 @@
 
 	let checkedRange = $state('Weekly');
 
-	const overviewItems = [
-		{
-			title: 'Total Transaction',
-			value: '37K',
-			sub: {
-				value: '+300',
-				description: 'from last week'
-			}
-		},
-		{
-			title: 'Total Wasted',
-			value: '50',
-			sub: {
-				value: '-3',
-				description: 'from last week'
-			}
-		},
-		{
-			title: 'Total Used',
-			value: '35K',
-			sub: {
-				value: '-1K',
-				description: 'from last week'
-			}
-		},
-		{
-			title: 'Stock Under Minimum',
-			value: '4 Material',
-			sub: {
-				value: '-1K',
-				description: 'from last week'
-			}
-		},
-		{
-			title: 'Active Borrowing',
-			value: '8 Transaction',
-			sub: {
-				value: '-1K',
-				description: 'from currnet shift'
-			}
+	const dashboard = () => {
+		let totalBorrowed = $state(0);
+		let totalUsedStock = $state(0);
+		let totalWastedStock = $state(0);
+		let totalStockUnderMinimum = $state(0);
+		let activeBorrowing = $state(0);
+		let materialMaintain = $state(0);
+		let frequentlyUsed = $state(0);
+		let frequentlyBorrowed = $state(0);
+		async function getTotalBorrowed() {
+			totalBorrowed = (await pb.collection('borrow_movement').getFullList({ sort: '-created' })).length;
 		}
-	];
+
+		async function getUsedStock() {
+			totalUsedStock = (await pb.collection('stock_out').getFullList({ filter: 'refference_id != ""' })).length;
+		}
+
+		async function getWastedStock() {
+			totalWastedStock = (await pb.collection('stock_out').getFullList({ filter: 'refference_id = ""' })).length;
+		}
+
+		async function getStockUnderMinimum() {
+			totalStockUnderMinimum = (
+				await pb.collection('stock_master').getFullList({
+					expand: 'material_id',
+					filter: 'quantity_available < material_id.minimum_quantity'
+				})
+			).length;
+		}
+
+		async function getActiveBorrowing() {
+			activeBorrowing = (await pb.collection('borrow_movement').getFullList({ sort: '-created', filter: 'status="OPEN"' })).length;
+		}
+
+		async function getMaterialMaintain() {
+			let groupMap = new Map();
+			let allItems = await pb.collection('stock_master').getFullList({ sort: 'material_id' });
+
+			for (let { material_id } of allItems) {
+				if (!groupMap.has(material_id)) groupMap.set(material_id, []);
+				groupMap.get(material_id).push(material_id);
+			}
+
+			materialMaintain = groupMap.size;
+		}
+
+		async function getFrequentlyUsed() {
+			let groupMap = new Map();
+			let allUsed = await pb.collection('stock_out').getFullList({ expand: 'stock_id' });
+
+			for (let { stock_id, quantity, expand } of allUsed) {
+				if (!groupMap.has(stock_id)) groupMap.set(stock_id, []);
+				groupMap.get(stock_id).push({ quantity, stock_id, material_id: expand?.stock_id.material_id });
+			}
+
+			let newx = [...groupMap.values()].map((conditions: { quantity: number; material_id: string }[]) => {
+				const merged = Object.values(
+					conditions.reduce<{ [key: string]: { quantity: number; material_id: string } }>((acc, { material_id, quantity }) => {
+						if (!acc[material_id]) {
+							acc[material_id] = { material_id, quantity: 0 };
+						}
+						acc[material_id].quantity += quantity;
+						return acc;
+					}, {})
+				);
+
+				return merged[0];
+			});
+			// Find the max quantity
+			const maxQuantity = Math.max(...newx.map((item) => item.quantity));
+
+			// Filter the items that have the max quantity
+			const result = newx.filter((item) => item.quantity === maxQuantity);
+			frequentlyUsed = result.length;
+		}
+
+		async function getFrequentlyBorrowed() {
+			let groupMap = new Map();
+			let allBorrowed = await pb.collection('borrow_item').getFullList({ expand: 'stock_id' });
+
+			for (let { stock_id, quantity_out, expand } of allBorrowed) {
+				if (!groupMap.has(stock_id)) groupMap.set(stock_id, []);
+				groupMap.get(stock_id).push({ quantity_out, stock_id, material_id: expand?.stock_id.material_id });
+			}
+
+			let newx = [...groupMap.values()].map((conditions: { quantity_out: number; material_id: string }[]) => {
+				const merged = Object.values(
+					conditions.reduce<{ [key: string]: { quantity_out: number; material_id: string } }>((acc, { material_id, quantity_out }) => {
+						if (!acc[material_id]) {
+							acc[material_id] = { material_id, quantity_out: 0 };
+						}
+						acc[material_id].quantity_out += quantity_out;
+						return acc;
+					}, {})
+				);
+
+				return merged[0];
+			});
+			// Find the max quantity
+			const maxQuantityOut = Math.max(...newx.map((item) => item.quantity_out));
+
+			// Filter the items that have the max quantity
+			const result = newx.filter((item) => item.quantity_out === maxQuantityOut);
+			frequentlyBorrowed = result.length;
+		}
+
+		let overviewItems = [
+			{
+				title: 'Stock Under Minimum',
+				value: () => totalStockUnderMinimum,
+				unit: 'Varian',
+				sub: {
+					value: '-1K',
+					description: 'go to detail'
+				}
+			},
+
+			{
+				title: 'Total Wasted',
+				value: () => totalWastedStock,
+				unit: 'EA',
+				sub: {
+					value: '-3',
+					description: 'from last week'
+				}
+			},
+			{
+				title: 'Total Used',
+				value: () => totalUsedStock,
+				unit: 'EA',
+				sub: {
+					value: '-1K',
+					description: 'from last week'
+				}
+			},
+			{
+				title: 'Total Borrow',
+				value: () => totalBorrowed,
+				unit: 'EA',
+				sub: {
+					value: '+300',
+					description: 'go to detail'
+				}
+			},
+			{
+				title: 'Active Borrowing',
+				value: () => activeBorrowing,
+				unit: 'Transaction',
+				sub: {
+					value: '-1K',
+					description: 'from currnet shift'
+				}
+			},
+			{
+				title: 'Material Maintain',
+				value: () => materialMaintain,
+				unit: 'Type',
+				sub: {
+					value: '+300',
+					description: 'go to detail'
+				}
+			},
+			{
+				title: 'Frequently Used',
+				value: () => frequentlyUsed,
+				unit: 'Type',
+				sub: {
+					value: '+300',
+					description: 'go to detail'
+				}
+			},
+			{
+				title: 'Frequently Borrowed',
+				value: () => frequentlyBorrowed,
+				unit: 'Type',
+				sub: {
+					value: '+300',
+					description: 'go to detail'
+				}
+			}
+		];
+
+		return {
+			get totalBorrowed() {
+				return totalBorrowed;
+			},
+
+			getTotalBorrowed,
+			getUsedStock,
+			getWastedStock,
+			getStockUnderMinimum,
+			getActiveBorrowing,
+			getMaterialMaintain,
+			getFrequentlyUsed,
+			getFrequentlyBorrowed,
+			overviewItems
+		};
+	};
+
+	let dashboardData = dashboard();
+
+	onMount(async () => {
+		dashboardData.getTotalBorrowed();
+		dashboardData.getUsedStock();
+		dashboardData.getWastedStock();
+		dashboardData.getStockUnderMinimum();
+		dashboardData.getActiveBorrowing();
+		dashboardData.getMaterialMaintain();
+		dashboardData.getFrequentlyUsed();
+		dashboardData.getFrequentlyBorrowed();
+	});
 </script>
 
 <svelte:head>
@@ -78,7 +250,7 @@
 		<div>
 			<hr role="presentation" class="w-full border-t" />
 			<div class="mt-6 text-lg font-medium sm:text-sm/6">Total Borrow</div>
-			<div class="mt-3 text-3xl/8 font-semibold sm:text-2xl/8">50 Transaction</div>
+			<div class="mt-3 text-3xl/8 font-semibold sm:text-2xl/8">{dashboardData.totalBorrowed}</div>
 			<div class="mt-3 text-sm/6 sm:text-xs/6">
 				<span class="inline-flex items-center gap-x-1.5 rounded-md bg-lime-400/20 px-1.5 py-0.5 text-sm/5 font-medium text-lime-700 group-data-[hover]:bg-lime-400/30 dark:bg-lime-400/10 dark:text-lime-300 dark:group-data-[hover]:bg-lime-400/15 sm:text-xs/5 forced-colors:outline">Count since</span> <span class="text-zinc-500">{time(new Date())}</span>
 			</div>
@@ -129,14 +301,23 @@
 		</div>
 	</div>
 	<div class="mt-4 grid gap-8 sm:grid-cols-2 xl:grid-cols-4">
-		{#each overviewItems as item}
+		{#each dashboardData.overviewItems as item}
 			<div>
 				<hr role="presentation" class="w-full border-t" />
 				<div class="mt-6 text-lg font-medium sm:text-sm/6">{item.title}</div>
-				<div class="mt-3 text-3xl/8 font-semibold sm:text-2xl/8">{item.value}</div>
-				<div class="mt-3 text-sm/6 sm:text-xs/6">
-					<span class="inline-flex items-center gap-x-1.5 rounded-md bg-lime-400/20 px-1.5 py-0.5 text-sm/5 font-medium text-lime-700 group-data-[hover]:bg-lime-400/30 dark:bg-lime-400/10 dark:text-lime-300 dark:group-data-[hover]:bg-lime-400/15 sm:text-xs/5 forced-colors:outline">{item.sub.value}</span> <span class="text-zinc-500">{item.sub.description}</span>
+				<div class="mt-3 text-3xl/8 font-semibold sm:text-2xl/8">
+					<NumberFlow value={item.value()} />
+					<span class="text-sm text-foreground/80">
+						{item.unit}
+					</span>
 				</div>
+				<!-- @TODO: add details button -->
+				<!-- <div class="mt-3 text-sm/6 sm:text-xs/6">
+					<span
+						class="inline-flex items-center gap-x-1.5 rounded-md px-1.5 py-0.5 text-sm/5 font-medium sm:text-xs/5 forced-colors:outline {item.value() == 0
+							? 'bg-lime-400/20 text-red-700 group-data-[hover]:bg-red-400/30 dark:bg-red-400/10 dark:text-red-300 dark:group-data-[hover]:bg-red-400/15'
+							: 'bg-lime-400/20 text-lime-700 group-data-[hover]:bg-lime-400/30 dark:bg-lime-400/10 dark:text-lime-300 dark:group-data-[hover]:bg-lime-400/15'}">{item.sub.value}</span> <span class="text-zinc-500">{item.sub.description}</span>
+				</div> -->
 			</div>
 		{/each}
 	</div>
@@ -148,7 +329,7 @@
 		<div>
 			<hr role="presentation" class="mb-4 w-full border-t" />
 
-			<RecentTransaction />
+			<!-- <RecentTransaction /> -->
 		</div>
 	</div>
 {/if}
