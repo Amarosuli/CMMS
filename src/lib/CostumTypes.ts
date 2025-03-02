ea59ac2431b43317c38dccb54dd240e36b755675
Amarosuli <vuvlnvw@gmail.com>
2025-03-03 01:54:53 +0700

    [update] User interface and add UserUnit and UserRole enum

--- src/lib/CostumTypes.ts
+++ src/lib/CostumTypes.ts
@@ -30,8 +30,10 @@ export interface User extends RecordModel {
 	email: string;
 	name: string;
 	avatar: string;
-	role: 'Admin' | 'General' | 'Super';
-	unit: string;
+	verified: boolean;
+	emailVisibility: true;
+	role: UserRole;
+	unit: UserUnit;
 }
 
 enum StockMasterStatus {
@@ -96,6 +98,37 @@ export enum BorrowStatus {
 	OPEN = 'OPEN',
 	PENDING = 'PENDING',
 	CLOSED = 'CLOSED'
+}
+
+export enum UserUnit {
+	TVP = 'TVP',
+	TVP1 = 'TVP-1',
+	TVP2 = 'TVP-2',
+	TVP3 = 'TVP-3',
+	TVP4 = 'TVP-4',
+	TVP5 = 'TVP-5',
+	TVP6 = 'TVP-6',
+	TVE = 'TVE',
+	TVE1 = 'TVE-1',
+	TVE2 = 'TVE-2',
+	TVE3 = 'TVE-3',
+	TVE4 = 'TVE-4',
+	TVE5 = 'TVE-5',
+	TVE6 = 'TVE-6',
+	TVU = 'TVU',
+	TVU1 = 'TVU-1',
+	TVU2 = 'TVU-2',
+	TVU3 = 'TVU-3',
+	TVU4 = 'TVU-4',
+	TVU5 = 'TVU-5',
+	TVU6 = 'TVU-6',
+	TVW = 'TVW'
+}
+
+export enum UserRole {
+	General = 'General',
+	Admin = 'Admin',
+	Super = 'Super'
 }
 
 export interface BorrowMovement extends RecordModel {
