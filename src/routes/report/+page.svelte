eb28f26b1c5f24c2bb0e7220394f61c5c4d466d3
amarosuli <vuvlnvw@gmail.com>
2026-04-02 12:10:28 +0700

    Tidy Up

--- src/routes/report/+page.svelte
+++ src/routes/report/+page.svelte
@@ -1,78 +1,14 @@
 <script lang="ts">
-	import { getLocalTimeZone, startOfWeek, startOfMonth, endOfWeek, endOfMonth, today, type DateValue } from '@internationalized/date';
-	import { getBorrowingReport } from './report.remote';
+	import { getLocalTimeZone, startOfWeek, startOfMonth, endOfWeek, endOfMonth, today } from '@internationalized/date';
+	import { GetBorrowingReport } from './report.remote';
 	import { RangeCalendar } from '$lib/components/ui/range-calendar/index.js';
+	import { LoaderCircle } from '@lucide/svelte';
 	import { onMount } from 'svelte';
 	import { Button } from '$lib/components/ui/button';
-   import ExcelJS from 'exceljs';
-
-
+	import { toast } from 'svelte-sonner';
+	import ExcelJS from 'exceljs';
 	import type { DateRange } from 'bits-ui';
-	import { LoaderCircle } from '@lucide/svelte';
 
-
-	async function exportToExcel(data: any[]) {
-		const workbook = new ExcelJS.Workbook();
-      const response = await fetch('/Template_R-043_R0.xlsx');
-      const arrayBuffer = await response.arrayBuffer();
-
-      await workbook.xlsx.load(arrayBuffer);
-
-      const worksheet = workbook.getWorksheet(1); 
-
-      if (!worksheet) {
-         console.error('Worksheet not found in the template!');
-         return;
-      }
-
-        worksheet.getCell('L2').value = `${value.start} to ${value.end}`;
-        worksheet.pageSetup.printTitlesRow = '1:9';
-        worksheet.pageSetup.printArea = `A1:M${10 + data.length}`;
-      
-      let startRow = 10; 
-
-      data.forEach((item, index) => {
-         const currentRow = worksheet.getRow(startRow + index);
-         
-         currentRow.getCell(1).value = index + 1;         // Col A
-         currentRow.getCell(2).value = item.date;         // Col B
-         currentRow.getCell(3).value = item.requester;    // Col C
-         currentRow.getCell(4).value = item.idNo;         // Col D
-         currentRow.getCell(5).value = item.partNumber;   // Col E
-         currentRow.getCell(6).value = item.description;  // Col F
-         currentRow.getCell(7).value = item.qty;          // Col G
-         currentRow.getCell(8).value = item.po;           // Col H
-         currentRow.getCell(9).value = item.bn;           // Col I
-         currentRow.getCell(10).value = item.expiredDate || 'N/A';  // Col J
-         currentRow.getCell(11).value = item.condition;  // Col K
-         currentRow.getCell(12).value = item.orderNumber; // Col L
-         currentRow.getCell(13).value = item.returned; // Col M
-         
-         // currentRow.commit();
-	})
-   
-   const buffer = await workbook.xlsx.writeBuffer();
-   const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
-   const url = URL.createObjectURL(blob);
-   const link = document.createElement('a');
-
-   link.setAttribute('href', url);
-		link.setAttribute('download', `Borrowing Report ${value.start} to ${value.end}.xlsx`);
-		document.body.appendChild(link); // Append to the body temporarily
-
-		link.click();
-
-		document.body.removeChild(link); // Remove the link after clicking
-		URL.revokeObjectURL(url); // Free up the temporary URL resource
-
-      isExportLoading = false;
-}
-
-	const lastDayTime = '23:59:59.999Z';
-	const firstDayTime = '00:00:00.000Z';
-
-	let now = today(getLocalTimeZone());
-	let isLoading = $state(false);
 	type BorrowingReportItem = {
 		id: string;
 		date: any;
@@ -89,11 +25,21 @@
 		returned: any;
 	};
 
+	let isExportBtnActive = $state(false);
+	let isExportLoading = $state(false);
 	let borrowingReport = $state<BorrowingReportItem[]>([]);
+	let activePreset = $state<'today' | 'thisWeek' | 'thisMonth' | 'lastWeek' | 'lastMonth' | null>('today');
+	let isLoading = $state(false);
+	let now = today(getLocalTimeZone());
+
+	const lastDayTime = '23:59:59.999Z';
+	const firstDayTime = '00:00:00.000Z';
 
 	onMount(() => {
 		setRange('today');
 	});
+
+	let value = $state<DateRange>({ start: now, end: now });
 
 	function getValue() {
 		return value;
@@ -103,11 +49,19 @@
 		value = newValue;
 	}
 
-	let value = $state<DateRange>({ start: now, end: now });
+	let status: string = $state<'CLOSED' | 'OPEN'>('CLOSED');
+	let stringFilter = $derived.by(() => {
+		const statusFilter = `status="${status}"`;
+		if (!value.end || value.start?.compare(value.end) === 0) {
+			return `${statusFilter} && borrowDate >= "${value.start} ${firstDayTime}" && borrowDate <= "${value.start} ${lastDayTime}"`;
+		} else {
+			return `${statusFilter} && borrowDate >= "${value.start} ${firstDayTime}" && borrowDate <= "${value.end} ${lastDayTime}"`;
+		}
+	});
 
 	function setRange(preset: 'today' | 'thisWeek' | 'thisMonth' | 'lastWeek' | 'lastMonth') {
 		activePreset = preset;
-      isExportBtnActive = false
+		isExportBtnActive = false;
 
 		const ranges = {
 			today: [now, now],
@@ -121,20 +75,66 @@
 		setValue({ start, end });
 	}
 
-	let status: string = $state('CLOSED');
+	let cachedTemplate: ArrayBuffer | null = null;
 
-	let stringFilter = $derived.by(() => {
-		const statusFilter = `status="${status}"`;
-		if (!value.end || value.start?.compare(value.end) === 0) {
-			return `${statusFilter} && borrowDate >= "${value.start} ${firstDayTime}" && borrowDate <= "${value.start} ${lastDayTime}"`;
-		} else {
-			return `${statusFilter} && borrowDate >= "${value.start} ${firstDayTime}" && borrowDate <= "${value.end} ${lastDayTime}"`;
+	async function getTemplate() {
+		if (!cachedTemplate) {
+			const response = await fetch('/Template_R-043_R0.xlsx');
+			cachedTemplate = await response.arrayBuffer();
 		}
-	});
+		return cachedTemplate;
+	}
 
-	let isExportBtnActive = $state(false);
-	let isExportLoading = $state(false);
-	let activePreset = $state<'today' | 'thisWeek' | 'thisMonth' | 'lastWeek' | 'lastMonth' | null>('today');
+	async function exportToExcel(data: any[]) {
+		const workbook = new ExcelJS.Workbook();
+
+		await workbook.xlsx.load(await getTemplate());
+
+		const worksheet = workbook.getWorksheet(1);
+
+		if (!worksheet) {
+			toast.error('Worksheet not found in the template!');
+			isExportLoading = false;
+			return;
+		}
+
+		worksheet.getCell('L2').value = `${value.start} to ${value.end}`;
+		worksheet.pageSetup.printTitlesRow = '1:9';
+		worksheet.pageSetup.printArea = `A1:M${10 + data.length}`;
+
+		let startRow = 10;
+
+		data.forEach((item, index) => {
+			const currentRow = worksheet.getRow(startRow + index);
+
+			currentRow.getCell(1).value = index + 1; // Col A
+			currentRow.getCell(2).value = item.date; // Col B
+			currentRow.getCell(3).value = item.requester; // Col C
+			currentRow.getCell(4).value = item.idNo; // Col D
+			currentRow.getCell(5).value = item.partNumber; // Col E
+			currentRow.getCell(6).value = item.description; // Col F
+			currentRow.getCell(7).value = item.qty; // Col G
+			currentRow.getCell(8).value = item.po; // Col H
+			currentRow.getCell(9).value = item.bn; // Col I
+			currentRow.getCell(10).value = item.expiredDate || 'N/A'; // Col J
+			currentRow.getCell(11).value = item.condition; // Col K
+			currentRow.getCell(12).value = item.orderNumber; // Col L
+			currentRow.getCell(13).value = item.returned; // Col M
+		});
+
+		const buffer = await workbook.xlsx.writeBuffer();
+		const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+		const url = URL.createObjectURL(blob);
+		const link = document.createElement('a');
+
+		link.setAttribute('href', url);
+		link.setAttribute('download', `Borrowing Report ${value.start} to ${value.end}.xlsx`);
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
+		URL.revokeObjectURL(url);
+		isExportLoading = false;
+	}
 </script>
 
 <svelte:head>
@@ -146,14 +146,14 @@
 <p class="pt-2 font-bold">Choose Status</p>
 <div class="flex gap-2 pt-2">
 	<Button
-   class={status === 'OPEN' ? 'bg-primary dark:bg-primary-foreground text-foreground hover:bg-primary/90 dark:hover:bg-primary-foreground/90' : ''}
+		class={status === 'OPEN' ? 'bg-primary text-foreground hover:bg-primary/90 dark:bg-primary-foreground dark:hover:bg-primary-foreground/90' : ''}
 		variant="outline"
 		size="sm"
 		onclick={() => {
 			status = 'OPEN';
 		}}>Get Open Only</Button>
 	<Button
-   class={status === 'CLOSED' ? 'bg-primary dark:bg-primary-foreground text-foreground hover:bg-primary/90 dark:hover:bg-primary-foreground/90' : ''}
+		class={status === 'CLOSED' ? 'bg-primary text-foreground hover:bg-primary/90 dark:bg-primary-foreground dark:hover:bg-primary-foreground/90' : ''}
 		variant="outline"
 		size="sm"
 		onclick={() => {
@@ -161,40 +161,37 @@
 		}}>Get Closed Only</Button>
 </div>
 <p class="pt-2 font-bold">Choose Range</p>
-<div class="grid grid-cols-2 md:flex gap-2 pt-2">
+<div class="grid grid-cols-2 gap-2 pt-2 md:flex">
 	<Button
-   class={activePreset === 'today' ? 'bg-primary dark:bg-primary-foreground text-foreground hover:bg-primary/90 dark:hover:bg-primary-foreground/90' : ''}
+		class={activePreset === 'today' ? 'bg-primary text-foreground hover:bg-primary/90 dark:bg-primary-foreground dark:hover:bg-primary-foreground/90' : ''}
 		variant="outline"
 		size="sm"
 		onclick={() => {
 			setRange('today');
 		}}>Today</Button>
 	<Button
-   class={activePreset === 'thisWeek' ? 'bg-primary dark:bg-primary-foreground text-foreground hover:bg-primary/90 dark:hover:bg-primary-foreground/90' : ''}
+		class={activePreset === 'thisWeek' ? 'bg-primary text-foreground hover:bg-primary/90 dark:bg-primary-foreground dark:hover:bg-primary-foreground/90' : ''}
 		variant="outline"
 		size="sm"
 		onclick={() => {
 			setRange('thisWeek');
 		}}>This Week</Button>
 	<Button
-   class={activePreset === 'thisMonth' ? 'bg-primary dark:bg-primary-foreground text-foreground hover:bg-primary/90 dark:hover:bg-primary-foreground/90' : ''}
-
+		class={activePreset === 'thisMonth' ? 'bg-primary text-foreground hover:bg-primary/90 dark:bg-primary-foreground dark:hover:bg-primary-foreground/90' : ''}
 		variant="outline"
 		size="sm"
 		onclick={() => {
 			setRange('thisMonth');
 		}}>This Month</Button>
 	<Button
-   class={activePreset === 'lastWeek' ? 'bg-primary dark:bg-primary-foreground text-foreground hover:bg-primary/90 dark:hover:bg-primary-foreground/90' : ''}
-
+		class={activePreset === 'lastWeek' ? 'bg-primary text-foreground hover:bg-primary/90 dark:bg-primary-foreground dark:hover:bg-primary-foreground/90' : ''}
 		variant="outline"
 		size="sm"
 		onclick={() => {
 			setRange('lastWeek');
 		}}>Last Week</Button>
 	<Button
-   class={activePreset === 'lastMonth' ? 'bg-primary dark:bg-primary-foreground text-foreground hover:bg-primary/90 dark:hover:bg-primary-foreground/90' : ''}
-
+		class={activePreset === 'lastMonth' ? 'bg-primary text-foreground hover:bg-primary/90 dark:bg-primary-foreground dark:hover:bg-primary-foreground/90' : ''}
 		variant="outline"
 		size="sm"
 		onclick={() => {
@@ -203,37 +200,38 @@
 </div>
 <div class="w-fit pt-2">
 	<RangeCalendar
-      onValueChange={() => {
-        activePreset = null;
-      }}  
+		onValueChange={() => {
+			activePreset = null;
+		}}
 		bind:value={getValue, setValue}
 		class="rounded-md border" />
 </div>
 
-<div class="mt-8 flex flex-col lg:flex-row md:items-end items-center justify-between">
-	<h2 class="text-base/7 text-center md:text-start font-semibold">Summary of <span class="capitalize bg-foreground text-background py rounded-full text-sm px-3">{status.toLowerCase()}</span> Borrowing Records for <span class="bg-foreground text-background py rounded-full text-sm px-3">{value.start}</span> to <span class="bg-foreground text-background py rounded-full text-sm px-3">{value.end ? value.end : value.start}</span></h2>
+<div class="mt-8 flex flex-col items-center justify-between md:items-end lg:flex-row">
+	<h2 class="text-center text-base/7 font-semibold md:text-start">
+		Summary of <span class="py rounded-full bg-foreground px-3 text-sm text-background capitalize">{status.toLowerCase()}</span> Borrowing Records for <span class="py rounded-full bg-foreground px-3 text-sm text-background">{value.start}</span> to <span class="py rounded-full bg-foreground px-3 text-sm text-background">{value.end ? value.end : value.start}</span>
+	</h2>
 	<div class="mt-4 md:mt-0">
 		<Button
 			variant="outline"
 			size="sm"
 			onclick={() => {
 				isLoading = true;
-				getBorrowingReport(stringFilter)
+				GetBorrowingReport(stringFilter)
 					.then((report) => {
 						borrowingReport = report;
-						console.log(report);
 					})
 					.catch((er) => {
 						console.error(er);
 					})
 					.finally(() => {
 						isLoading = false;
-                  isExportBtnActive = borrowingReport.length > 0;
+						isExportBtnActive = borrowingReport.length > 0;
 					});
 			}}>
 			{#if isLoading}
 				Wait...
-				<LoaderCircle class="text-primary ml-2 h-4 w-4 animate-spin" />
+				<LoaderCircle class="ml-2 h-4 w-4 animate-spin text-primary" />
 			{:else}
 				Load Report
 			{/if}
@@ -244,11 +242,11 @@
 			size="sm"
 			onclick={() => {
 				isExportLoading = true;
-            exportToExcel(borrowingReport)
+				exportToExcel(borrowingReport);
 			}}>
 			{#if isExportLoading}
 				Wait...
-				<LoaderCircle class="text-primary ml-2 h-4 w-4 animate-spin" />
+				<LoaderCircle class="ml-2 h-4 w-4 animate-spin text-primary" />
 			{:else}
 				Export
 			{/if}
@@ -260,19 +258,19 @@
 		<hr role="presentation" class="mb-4 w-full border-t" />
 
 		{#each borrowingReport as item}
-			<div class="mb-2 flex flex-col md:flex-row gap-2 text-xs rounded-md border p-2 items-start">
-				<p class="md:w-3/12 bg-background wrap-break-word"> {item.date}</p>
-				<p class="md:w-2/12 bg-background wrap-break-word capitalize"> {item.requester.toLowerCase()}</p>
-				<p class="md:w-1/12 bg-background wrap-break-word"> {item.idNo}</p>
-				<p class="md:w-4/12 bg-background wrap-break-word"> {item.partNumber}</p>
-				<p class="md:w-6/12 bg-background wrap-break-word"> {item.description}</p>
-				<p class="md:w-1/12 bg-background wrap-break-word"> {item.qty}</p>
-				<p class="md:w-3/12 bg-background wrap-break-word"> {item.po}</p>
-				<p class="md:w-3/12 bg-background wrap-break-word"> {item.bn}</p>
-				<p class="md:w-3/12 bg-background wrap-break-word"> {item.expiredDate}</p>
-				<p class="md:w-2/12 bg-background wrap-break-word"> {item.condition}</p>
-				<p class="md:w-3/12 bg-background wrap-break-word"> {item.orderNumber}</p>
-				<p class="md:w-1/12 bg-background wrap-break-word"> {item.returned}</p>
+			<div class="mb-2 flex flex-col items-start gap-2 rounded-md border p-2 text-xs md:flex-row">
+				<p class="bg-background wrap-break-word md:w-3/12">{item.date}</p>
+				<p class="bg-background wrap-break-word capitalize md:w-2/12">{item.requester.toLowerCase()}</p>
+				<p class="bg-background wrap-break-word md:w-1/12">{item.idNo}</p>
+				<p class="bg-background wrap-break-word md:w-4/12">{item.partNumber}</p>
+				<p class="bg-background wrap-break-word md:w-6/12">{item.description}</p>
+				<p class="bg-background wrap-break-word md:w-1/12">{item.qty}</p>
+				<p class="bg-background wrap-break-word md:w-3/12">{item.po}</p>
+				<p class="bg-background wrap-break-word md:w-3/12">{item.bn}</p>
+				<p class="bg-background wrap-break-word md:w-3/12">{item.expiredDate}</p>
+				<p class="bg-background wrap-break-word md:w-2/12">{item.condition}</p>
+				<p class="bg-background wrap-break-word md:w-3/12">{item.orderNumber}</p>
+				<p class="bg-background wrap-break-word md:w-1/12">{item.returned}</p>
 			</div>
 		{/each}
 
--- src/routes/report/report.remote.ts
+++ src/routes/report/report.remote.ts
@@ -1,52 +1,14 @@
 import { getRequestEvent, query } from '$app/server';
 import { tryCatch } from '$lib/TryCatch';
-import { toast } from 'svelte-sonner';
-import dayjs from 'dayjs';
+import { string } from 'valibot';
 
-export const getBorrowingItems = query('unchecked', async (borrowId: string) => {
+export const GetBorrowingReport = query(string(), async (filter?: string) => {
 	const { locals } = getRequestEvent();
 
-	if (!borrowId) {
-		toast.error('Borrow ID is required');
-		return [];
-	}
-
-	const { data, error } = await tryCatch(locals.pb.collection('borrow_item').getFullList({ filter: 'borrow_id="' + borrowId + '"', expand: 'stock_id.material_id' }));
+	const { data, error } = await tryCatch(locals.pb.collection('borrow_report').getFullList({ filter: filter, expand: 'userId,stockId.material_master_id.material_unit_id' }));
 
 	if (error) {
-		toast.error('Failed to fetch borrowing items');
-		return [];
-	}
-
-	return data;
-});
-
-export const getBorrowingMovements = query('unchecked', async (filter: { status?: string; dateStart?: string; dateEnd?: string }) => {
-	const { locals } = getRequestEvent();
-	let status = filter.status ? filter.status : 'CLOSED';
-	let dateStart = filter.dateStart ? filter.dateStart : dayjs().subtract(1, 'day').format('YYYY-MM-DD');
-	let dateEnd = filter.dateEnd ? filter.dateEnd : dayjs().format('YYYY-MM-DD');
-
-	const { data, error } = await tryCatch(locals.pb.collection('borrow_movement').getList(1, 10, { filter: 'status="' + status + '" && created >= "' + dateStart + '" && created <= "' + dateEnd + '"', expand: 'user_id' }));
-
-	console.log(error);
-	if (error) {
-		toast.error('Failed to fetch borrowing report');
-		return [];
-	}
-
-	return data.items.map((r) => {
-		return { ...r, user: r.expand?.user_id || undefined };
-	});
-});
-
-export const getBorrowingReport = query('unchecked', async (filter?: string) => {
-	const { locals } = getRequestEvent();
-
-	const { data, error } = await tryCatch(locals.pb.collection('borrow_report').getFullList({ filter: filter, expand: 'userId,stockId.material_id.unit_id' }));
-
-	if (error) {
-		toast.error('Failed to fetch borrowing report');
+		console.log(error);
 		return [];
 	}
 
@@ -56,9 +18,9 @@ export const getBorrowingReport = query('unchecked', async (filter?: string) => 
 			date: r.borrowDate.split(' ')[0],
 			requester: r.expand?.userId.name.split(' ').slice(0, 2).join(' '),
 			idNo: r.expand?.userId.username,
-			partNumber: r.expand?.stockId.expand?.material_id.part_number,
-			description: r.expand?.stockId.expand?.material_id.description,
-			qty: r.qty + ' ' + r.expand?.stockId.expand?.material_id.expand?.unit_id.code,
+			partNumber: r.expand?.stockId.expand?.material_master_id.part_number,
+			description: r.expand?.stockId.expand?.material_master_id.description,
+			qty: r.qty + ' ' + r.expand?.stockId.expand?.material_master_id.expand?.material_unit_id.code,
 			po: r.expand?.stockId.purchase_order,
 			bn: r.expand?.stockId.batch_number,
 			expiredDate: r.expand?.stockId.expired_date.split(' ')[0],
