60309b80eaf837f6a7c55a49a6f625de51244006
amarosuli <vuvlnvw@gmail.com>
2026-05-08 21:29:35 +0700

    Update stock +page.svelte

--- src/routes/stock/+page.svelte
+++ src/routes/stock/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import * as ToggleGroup from '$lib/components/ui/toggle-group/index.js';
+	// import * as ToggleGroup from '$lib/components/ui/toggle-group/index.js';
 	import { type ColumnDef, type VisibilityState, getCoreRowModel, getSortedRowModel } from '@tanstack/table-core';
-	import { DataTableActions, DataTableSortColumn, ExportWidget, SuperTable, ViewBarcode } from '$lib/components/costum';
+	import { DataTableActions, DataTableSortColumn, ExportWidget, SuperTable, ViewAllStockQuantity } from '$lib/components/costum';
 	import { createSvelteTable, renderComponent, renderSnippet } from '$lib/components/ui/data-table/index.js';
 	import { createRawSnippet, onMount } from 'svelte';
 	import { createPageFile } from '$lib/PageTable.svelte';
@@ -11,56 +11,45 @@
 	const { user } = $derived(data);
 
 	const pageFile = createPageFile({
-		collectionName: 'stock_master',
+		collectionName: 'stock_overview',
 		perPage: 10,
-		// svelte-ignore state_referenced_locally
-		options: { expand: 'material_master_id.material_unit_id', filter: 'status="ACTIVE"' }
-	});
-
-	pageFile.addModifier((items: any[]) => {
-		return items.map((val) => {
-			return { ...val, materialData: val.expand ? val.expand.material_master_id : 'N/A' };
-		});
-	});
-
-	$effect(() => {
-		console.log('Page file items updated:', pageFile.items);
+		options: { expand: 'material_unit_id' }
 	});
 
 	export const columns: ColumnDef<(typeof pageFile)['items'][number]>[] = [
 		{
-			accessorKey: 'material_master_id.description',
+			accessorKey: 'description',
 			id: 'Description',
-			header: ({ column }) =>
+			header: () =>
 				renderComponent(DataTableSortColumn, {
 					text: 'Material Description',
 					disabled: pageFile.isLoading,
-					direction: pageFile.sortBucket === 'material_master_id.description' ? pageFile.sortDirection : undefined,
-					onclick: () => pageFile.sort('material_master_id.description')
+					direction: pageFile.sortBucket === 'description' ? pageFile.sortDirection : undefined,
+					onclick: () => pageFile.sort('description')
 				}),
 			cell: ({ row }) => {
 				const Snippet = createRawSnippet<[string]>(() => {
-					const data = row.original.expand?.material_master_id.description.toLowerCase();
+					const data = row.original.description;
 					return {
-						render: () => `<div class="capitalize">${data}</div>`
+						render: () => `<div class="capitalize w-40 text-wrap">${data}</div>`
 					};
 				});
 				return renderSnippet(Snippet);
 			}
 		},
 		{
-			accessorKey: 'material_master_id.part_number',
+			accessorKey: 'part_number',
 			id: 'Part Number',
-			header: ({ column }) =>
+			header: () =>
 				renderComponent(DataTableSortColumn, {
 					text: 'Part Number',
 					disabled: pageFile.isLoading,
-					direction: pageFile.sortBucket === 'material_master_id.part_number' ? pageFile.sortDirection : undefined,
-					onclick: () => pageFile.sort('material_master_id.part_number')
+					direction: pageFile.sortBucket === 'part_number' ? pageFile.sortDirection : undefined,
+					onclick: () => pageFile.sort('part_number')
 				}),
 			cell: ({ row }) => {
 				const Snippet = createRawSnippet<[string]>(() => {
-					const data = row.original.expand?.material_master_id.part_number.toLowerCase();
+					const data = row.original.part_number.toLowerCase();
 					return {
 						render: () => `<div class="uppercase">${data}</div>`
 					};
@@ -69,73 +58,49 @@
 			}
 		},
 		{
-			accessorKey: 'batch_number',
-			id: 'Batch Number',
-			header: ({ column }) =>
+			accessorKey: 'batches',
+			id: 'Batch Numbers',
+			header: () =>
 				renderComponent(DataTableSortColumn, {
-					text: 'Batch Number',
+					text: 'Batch Numbers',
 					disabled: pageFile.isLoading,
-					direction: pageFile.sortBucket === column.id ? pageFile.sortDirection : undefined,
-					onclick: () => pageFile.sort(column.id)
+					direction: pageFile.sortBucket === 'batches' ? pageFile.sortDirection : undefined,
+					onclick: () => pageFile.sort('batches')
 				}),
 			cell: ({ row }) => {
-				return row.original.batch_number;
+				const Snippet = createRawSnippet<[string]>(() => {
+					const data = row.original.batches.toString(); // should make sure it's a string, so split method can be applied without errors
+					return {
+						render: () => `<div class="capitalize w-40 text-wrap">${data.split(',').join(', ')}</div>`
+					};
+				});
+				return renderSnippet(Snippet);
 			}
 		},
 		{
-			accessorKey: 'quantity_available',
-			id: 'Quantity Available',
-			header: ({ column }) =>
+			accessorKey: 'total_quantity',
+			id: 'Total Quantity',
+			header: () =>
 				renderComponent(DataTableSortColumn, {
-					text: 'Available Quantity',
+					text: 'Total Quantity',
 					disabled: pageFile.isLoading,
-					direction: pageFile.sortBucket === 'quantity_available' ? pageFile.sortDirection : undefined,
-					onclick: () => pageFile.sort('quantity_available')
+					direction: pageFile.sortBucket === 'total_quantity' ? pageFile.sortDirection : undefined,
+					onclick: () => pageFile.sort('total_quantity')
 				}),
 			cell: ({ row }) => {
-				let materialUnit = row.original.materialData?.expand?.unit_id?.code || 'EA';
-				return row.original.quantity_available + ' ' + materialUnit;
+				const Snippet = createRawSnippet<[string]>(() => {
+					const data = row.original.total_quantity; // should make sure it's a string, so split method can be applied without errors
+					return {
+						render: () => `<div class="capitalize w-40 text-wrap">${data} ${row.original.expand?.material_unit_id.code}</div>`
+					};
+				});
+				return renderSnippet(Snippet);
 			}
 		},
 		{
-			id: 'Barcode',
-			header: ({ column }) =>
-				renderComponent(DataTableSortColumn, {
-					text: 'Barcode',
-					disabled: pageFile.isLoading,
-					direction: pageFile.sortBucket === column.id ? pageFile.sortDirection : undefined
-				}),
+			header: 'Detail',
 			cell: ({ row }) => {
-				return renderComponent(ViewBarcode, { data: row.original });
-			}
-		},
-		{
-			accessorKey: 'quantity_borrowed',
-			id: 'Quantity Borrowed',
-			header: ({ column }) =>
-				renderComponent(DataTableSortColumn, {
-					text: 'Quantity Borrowed',
-					disabled: pageFile.isLoading,
-					direction: pageFile.sortBucket === 'quantity_borrowed' ? pageFile.sortDirection : undefined,
-					onclick: () => pageFile.sort('quantity_borrowed')
-				}),
-			cell: ({ row }) => {
-				let materialUnit = row.original.materialData?.expand?.unit_id?.code || 'EA';
-				return row.original.quantity_borrowed + ' ' + materialUnit;
-			}
-		},
-		{
-			accessorKey: 'material_master_id.code',
-			id: 'Code',
-			header: ({ column }) =>
-				renderComponent(DataTableSortColumn, {
-					text: 'Code',
-					disabled: pageFile.isLoading,
-					direction: pageFile.sortBucket === 'material_master_id.code' ? pageFile.sortDirection : undefined,
-					onclick: () => pageFile.sort('material_master_id.code')
-				}),
-			cell: ({ row }) => {
-				return row.original.materialData.code;
+				return renderComponent(ViewAllStockQuantity, { data: row.original });
 			}
 		},
 		{
@@ -173,18 +138,18 @@
 		pageFile.load();
 	});
 
-	let toggleFilter = $state('');
+	// let toggleFilter = $state('');
 
-	function changeFilter() {
-		if (toggleFilter === '1') {
-			pageFile.setFilter('status="INACTIVE"');
-		} else if (toggleFilter === '2') {
-			pageFile.setFilter('status="ACTIVE"||status="INACTIVE"');
-		} else {
-			pageFile.setFilter('');
-		}
-		pageFile.reload();
-	}
+	// function changeFilter() {
+	// 	if (toggleFilter === '1') {
+	// 		pageFile.setFilter('status="INACTIVE"');
+	// 	} else if (toggleFilter === '2') {
+	// 		pageFile.setFilter('status="ACTIVE"||status="INACTIVE"');
+	// 	} else {
+	// 		pageFile.setFilter('');
+	// 	}
+	// 	pageFile.reload();
+	// }
 </script>
 
 <svelte:head>
@@ -192,13 +157,13 @@
 </svelte:head>
 
 <SuperTable config={{ tableName: 'Stock Available', addUrl: '/movement/stock-in' }} {table} {pageFile} {columns}>
-	<ToggleGroup.Root class="hidden lg:block" variant="outline" type="single" bind:value={toggleFilter} onValueChange={changeFilter}>
+	<!-- <ToggleGroup.Root class="hidden lg:block" variant="outline" type="single" bind:value={toggleFilter} onValueChange={changeFilter}>
 		<ToggleGroup.Item value="1" aria-label="Toggle bold">
 			<p class="px-5">Inactive</p>
 		</ToggleGroup.Item>
 		<ToggleGroup.Item value="2" aria-label="Toggle italic">
 			<p class="px-5">All</p>
 		</ToggleGroup.Item>
-	</ToggleGroup.Root>
+	</ToggleGroup.Root> -->
 	<ExportWidget {pageFile} />
 </SuperTable>
