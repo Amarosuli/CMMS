9f3ac1258808ec2cdc24aa75b0d5530296fbcc7d
amarosuli <vuvlnvw@gmail.com>
2026-05-08 21:30:03 +0700

    Update detail stock

--- src/routes/stock/[id]/+page.svelte
+++ src/routes/stock/[id]/+page.svelte
@@ -1,79 +1,17 @@
 <script lang="ts">
-	import { ChevronLeft, CalendarPlus } from '@lucide/svelte';
-	import { barcodes, text, rectangle } from '@pdfme/schemas';
-	import { barcodeListTemplate } from '$lib/components/costum/barcodeListTemplate.js';
-	import { generate } from '@pdfme/generator';
+	import { ChevronLeft, CalendarPlus, Pencil, Trash2, Barcode } from '@lucide/svelte';
 	import { Button } from '$lib/components/ui/button';
-	import { toast } from 'svelte-sonner';
-	import { time } from '$lib/helpers.js';
-	import { goto } from '$app/navigation';
-	import { page } from '$app/state';
+
+	import type { StockMaster } from '$lib/CostumTypes';
+	import AddToLabelCart from '$lib/components/costum/AddToLabelCart.svelte';
 
 	let { data } = $props();
+	// let isLoading = $state(false);
 
-	const { stockMaster } = $derived(data);
+	const { allStock } = $derived(data);
 
-	let material = stockMaster?.expand?.material_id ? { ...stockMaster.expand.material_id } : { code: '-', part_number: '-', description: '-' };
-	let unit = stockMaster?.expand?.material_id?.expand?.unit_id ? { code: stockMaster.expand.material_id.expand.unit_id.code } : { code: '' };
-	let qtyAvailableInStore = stockMaster?.quantity_borrowed && stockMaster?.quantity_available ? `: ${stockMaster.quantity_available - stockMaster.quantity_borrowed} ${unit.code}` : '';
-
-	let isLoading = $state(false);
-	let infoState = $state('');
-	async function getPDFBarcodeList() {
-		isLoading = true;
-		infoState = 'Generate PDF ...';
-		let resultObjectArray: Record<string, string>[] = [];
-		let currentObject: Record<string, string> = {};
-		let keyIndex = 1;
-
-		if (!data.stockMaster) return;
-		const { quantity_available, batch_number } = data.stockMaster;
-
-		if (batch_number && quantity_available) {
-			for (let index = 0; index < quantity_available; index++) {
-				const key = `{${keyIndex}}bn`;
-				currentObject[key] = batch_number;
-				if (keyIndex === 100) {
-					resultObjectArray.push(currentObject);
-					currentObject = {};
-					keyIndex = 1;
-				} else {
-					keyIndex++;
-				}
-			}
-			if (Object.keys(currentObject).length > 0) {
-				resultObjectArray.push(currentObject);
-			}
-		} else {
-			toast.info('Material not available in store');
-			infoState = '';
-			isLoading = false;
-			return;
-		}
-
-		const template = barcodeListTemplate;
-		const inputs = resultObjectArray;
-		const plugins = { code128: barcodes.code128, text: text, rectangle: rectangle };
-
-		generate({ template, inputs, plugins })
-			.then((pdf) => {
-				const blob = new Blob([pdf.buffer as BlobPart], { type: 'application/pdf' });
-				const url = URL.createObjectURL(blob);
-				const link = document.createElement('a');
-
-				link.setAttribute('href', url);
-				link.setAttribute('download', `${data.stockMaster?.batch_number} Barcode List.pdf`);
-				document.body.appendChild(link); // Append to the body temporarily
-
-				link.click();
-
-				document.body.removeChild(link); // Remove the link after clicking
-				URL.revokeObjectURL(url); // Free up the temporary URL resource
-			})
-			.finally(() => {
-				infoState = '';
-				isLoading = false;
-			});
+	function getQtyAvailableInStore(stock: StockMaster) {
+		return stock.quantity_available ? `${stock.quantity_available - stock.quantity_borrowed} ${stock.expand?.material_master_id.expand.material_unit_id.code}` : '';
 	}
 </script>
 
@@ -97,35 +35,45 @@
 		<div class="flex flex-wrap gap-x-10 gap-y-4 py-1.5">
 			<span class="flex items-center gap-3 text-base/6 sm:text-sm/6">
 				<CalendarPlus class="h-4 w-4" />
-				<span>{time(data.stockMaster?.created)}</span></span>
+				<!-- <span>{time(data.stockMaster?.created)}</span> -->
+			</span>
 		</div>
-		<div class="flex gap-4">
-			<Button variant="outline" onclick={() => goto(page.url.pathname + '/edit')} class="min-w-20 ">Edit</Button>
-			<Button variant="outline" onclick={getPDFBarcodeList} class="min-w-20 ">
-				{infoState || 'Print Barcode'}</Button>
-		</div>
+		<div class="flex gap-4"></div>
 	</div>
 </div>
 
 <div class="mt-12">
 	<h2 class="text-base/7 font-semibold text-foreground sm:text-sm/6">Detail of Stock</h2>
-	<hr role="presentation" class="mt-4 w-full border-t border-foreground/10" />
-	<dl class="grid grid-cols-1 text-base/6 sm:grid-cols-[min(50%,--spacing(80))_auto] sm:text-sm/6">
-		<dt class="col-start-1 border-t border-foreground/5 pt-3 text-foreground/50 first:border-none sm:py-3">Batch Number</dt>
-		<dd class="pt-1 pb-3 text-foreground sm:border-t sm:border-foreground/5 sm:py-3 sm:nth-2:border-none">{data.stockMaster?.batch_number}</dd>
-		<dt class="col-start-1 border-t border-foreground/5 pt-3 text-foreground/50 first:border-none sm:py-3">Purchase Order</dt>
-		<dd class="pt-1 pb-3 text-foreground sm:border-t sm:border-foreground/5 sm:py-3 sm:nth-2:border-none">{data.stockMaster?.purchase_order}</dd>
-		<dt class="col-start-1 border-t border-foreground/5 pt-3 text-foreground/50 first:border-none sm:py-3">Available Quantity</dt>
-		<dd class="pt-1 pb-3 text-foreground sm:border-t sm:border-foreground/5 sm:py-3 sm:nth-2:border-none">{data.stockMaster?.quantity_available} {unit?.code} ( Available in store{qtyAvailableInStore} )</dd>
-		<dt class="col-start-1 border-t border-foreground/5 pt-3 text-foreground/50 first:border-none sm:py-3">Borrowed Quantity</dt>
-		<dd class="pt-1 pb-3 text-foreground sm:border-t sm:border-foreground/5 sm:py-3 sm:nth-2:border-none">{data.stockMaster?.quantity_borrowed} {unit.code}</dd>
-		<dt class="col-start-1 border-t border-foreground/5 pt-3 text-foreground/50 first:border-none sm:py-3">Expired Date</dt>
-		<dd class="pt-1 pb-3 text-foreground sm:border-t sm:border-foreground/5 sm:py-3 sm:nth-2:border-none">{data.stockMaster?.expired_date}</dd>
-		<dt class="col-start-1 border-t border-foreground/5 pt-3 text-foreground/50 first:border-none sm:py-3">Material Code</dt>
-		<dd class="pt-1 pb-3 text-foreground sm:border-t sm:border-foreground/5 sm:py-3 sm:nth-2:border-none">{material.code}</dd>
-		<dt class="col-start-1 border-t border-foreground/5 pt-3 text-foreground/50 first:border-none sm:py-3">Material Part Number</dt>
-		<dd class="pt-1 pb-3 text-foreground sm:border-t sm:border-foreground/5 sm:py-3 sm:nth-2:border-none">{material.part_number}</dd>
-		<dt class="col-start-1 border-t border-foreground/5 pt-3 text-foreground/50 first:border-none sm:py-3">Material Description</dt>
-		<dd class="pt-1 pb-3 text-foreground sm:border-t sm:border-foreground/5 sm:py-3 sm:nth-2:border-none">{material.description}</dd>
-	</dl>
+	{#if allStock.items.length}
+		{#each allStock.items as stock (stock.id)}
+			<div class="mt-4 overflow-x-auto pb-2">
+				<div class="flex min-w-max items-center justify-end gap-2">
+					<Button variant="outline" size="sm" class="w-fit shrink-0 rounded-md bg-lime-400/20 px-3 py-0.5 text-center text-sm/5 font-medium text-lime-700  sm:text-xs/5 dark:bg-lime-400/10 dark:text-lime-300 forced-colors:outline">{stock.batch_number}</Button>
+					<AddToLabelCart data={stock} />
+					<Button variant="outline" size="sm" class="shrink-0 text-xs"><Barcode class="size-4" />Print Label</Button>
+					<Button variant="outline" size="sm" class="shrink-0 text-xs"><Pencil class="size-4" /> Edit</Button>
+					<Button variant="outline" size="sm" class="shrink-0 text-xs"><Trash2 class="size-4" /> Delete</Button>
+				</div>
+			</div>
+			<hr role="presentation" class="w-full border-t border-foreground/10" />
+			<dl class="grid grid-cols-1 text-base/6 sm:grid-cols-[min(50%,--spacing(80))_auto] sm:text-sm/6">
+				<dt class="col-start-1 border-t border-foreground/5 pt-2 text-foreground/50 first:border-none sm:py-2">Batch Number</dt>
+				<dd class=" pb-2 text-foreground sm:border-t sm:border-foreground/5 sm:py-2 sm:nth-2:border-none">{stock.batch_number}</dd>
+				<dt class="col-start-1 border-t border-foreground/5 pt-2 text-foreground/50 first:border-none sm:py-2">Purchase Order</dt>
+				<dd class=" pb-2 text-foreground sm:border-t sm:border-foreground/5 sm:py-2 sm:nth-2:border-none">{stock.purchase_order}</dd>
+				<dt class="col-start-1 border-t border-foreground/5 pt-2 text-foreground/50 first:border-none sm:py-2">Available Quantity</dt>
+				<dd class=" pb-2 text-foreground sm:border-t sm:border-foreground/5 sm:py-2 sm:nth-2:border-none">{stock.quantity} {stock.expand?.material_master_id.expand.material_unit_id.code || '-'} ( Available in store {getQtyAvailableInStore(stock)} )</dd>
+				<dt class="col-start-1 border-t border-foreground/5 pt-2 text-foreground/50 first:border-none sm:py-2">Borrowed Quantity</dt>
+				<dd class=" pb-2 text-foreground sm:border-t sm:border-foreground/5 sm:py-2 sm:nth-2:border-none">{stock.quantity_borrowed} {stock.expand?.material_master_id.expand.material_unit_id.code || '-'}</dd>
+				<dt class="col-start-1 border-t border-foreground/5 pt-2 text-foreground/50 first:border-none sm:py-2">Expired Date</dt>
+				<dd class=" pb-2 text-foreground sm:border-t sm:border-foreground/5 sm:py-2 sm:nth-2:border-none">{stock.expired_date}</dd>
+				<dt class="col-start-1 border-t border-foreground/5 pt-2 text-foreground/50 first:border-none sm:py-2">Material Code</dt>
+				<dd class=" pb-2 text-foreground sm:border-t sm:border-foreground/5 sm:py-2 sm:nth-2:border-none">{stock.expand?.material_master_id.expand.material_unit_id.code || '-'}</dd>
+				<dt class="col-start-1 border-t border-foreground/5 pt-2 text-foreground/50 first:border-none sm:py-2">Material Part Number</dt>
+				<dd class=" pb-2 text-foreground sm:border-t sm:border-foreground/5 sm:py-2 sm:nth-2:border-none">{stock.expand?.material_master_id.part_number || '-'}</dd>
+				<dt class="col-start-1 border-t border-foreground/5 pt-2 text-foreground/50 first:border-none sm:py-2">Material Description</dt>
+				<dd class=" pb-2 text-foreground sm:border-t sm:border-foreground/5 sm:py-2 sm:nth-2:border-none">{stock.expand?.material_master_id.description || '-'}</dd>
+			</dl>
+		{/each}
+	{/if}
 </div>
