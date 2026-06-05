cd2e9be057d63250e5b2399d8c6e6a0fac13cf86
amarosuli <vuvlnvw@gmail.com>
2026-06-05 17:53:29 +0700

    Update stock-out page

--- src/routes/movement/stock-out/+page.server.ts
+++ src/routes/movement/stock-out/+page.server.ts
@@ -1,9 +1,8 @@
 import { fail, message, superValidate } from 'sveltekit-superforms';
 import { StockOutSchema } from '$lib/valibotSchema.js';
+import { tryCatch } from '$lib/TryCatch.js';
 import { redirect } from '@sveltejs/kit';
 import { valibot } from 'sveltekit-superforms/adapters';
-
-import type { RecordModel } from 'pocketbase';
 
 export const load = async ({ locals }) => {
 	if (!locals.user) throw redirect(302, '/'); // Prevent guest users from accessing this page directly.
@@ -18,14 +17,13 @@ export const actions = {
 
 		if (!form.valid) return fail(400, { form });
 
-		let result: RecordModel;
-		try {
-			result = await locals.pb.collection('stock_out').create(form.data);
-		} catch (er: any) {
-			const errorMessage = `${er?.response.message} | PocketBase error.`;
-			return message(form, errorMessage, { status: er?.status });
+		const { data, error } = await tryCatch(locals.pb.collection('stock_out').create(form.data));
+
+		if (error) {
+			const errorMessage = `${error?.message} | PocketBase error (Stock Out)`;
+			return message(form, errorMessage, { status: 500 });
 		}
 
-		return message(form, { text: 'Create Stock Out Successfully!', result });
+		return message(form, { text: 'Create Stock Out Successfully!', result: data });
 	}
 };
