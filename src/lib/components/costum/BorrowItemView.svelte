591c8406ac7ae847e108153ccb1e418e3dc73f40
amarosuli <vuvlnvw@gmail.com>
2026-06-05 17:52:24 +0700

    Update BorrowItemView component

--- src/lib/components/costum/BorrowItemView.svelte
+++ src/lib/components/costum/BorrowItemView.svelte
@@ -3,7 +3,7 @@
 	import { Pencil, Trash, LoaderCircle } from '@lucide/svelte';
 	import { Button } from '../ui/button';
 
-	import type { BorrowItem, BorrowMovement } from '$lib/CostumTypes';
+	import type { BorrowItem } from '$lib/CostumTypes';
 
 	interface Props {
 		item: BorrowItem;
@@ -26,18 +26,18 @@
 
 <div class="flex items-center gap-3 border-b py-2">
 	<div class="flex max-w-80 min-w-80 flex-col justify-center">
-		<p class="text-xs font-semibold">{item.material.code} - <span class="text-primary">{item.material.part_number}</span></p>
-		<p class="text-xs">{item.material.description}</p>
+		<p class="text-xs font-semibold">{item.expand?.stock_item_id.expand.stock_master_id.expand.material_master_id.code} - <span class="text-primary">{item.expand?.stock_item_id.expand.stock_master_id.expand.material_master_id.part_number}</span></p>
+		<p class="text-xs">{item.expand?.stock_item_id.expand.stock_master_id.expand.material_master_id.description}</p>
 	</div>
-	<p class="flex h-full min-w-20 flex-row gap-2 self-center text-sm">Qty : {item.quantity_out} {item.material.expand?.unit_id.code ?? ''}</p>
-	<p class="flex h-full min-w-20 flex-row gap-2 self-center text-sm">Qty : {item.quantity_return} {item.material.expand?.unit_id.code ?? ''}</p>
+	<p class="flex h-full min-w-20 flex-row gap-2 self-center text-sm">Qty : {item.quantity_out} {item.expand?.stock_item_id.expand.stock_master_id.expand.material_master_id.expand.material_unit_id.code ?? ''}</p>
+	<p class="flex h-full min-w-20 flex-row gap-2 self-center text-sm">Qty : {item.quantity_return} {item.expand?.stock_item_id.expand.stock_master_id.expand.material_master_id.expand.material_unit_id.code ?? ''}</p>
 	<div class="ml-auto flex min-w-40 flex-col items-center">
-		<p class="flex h-full flex-row gap-2 self-start text-xs">BN <span class="text-primary">{item.stock.batch_number}</span></p>
-		<p class="flex h-full flex-row gap-2 self-start text-xs">PO <span>{item.stock.purchase_order}</span></p>
+		<p class="flex h-full flex-row gap-2 self-start text-xs">BN <span class="text-primary">{item.expand?.stock_item_id.expand.stock_master_id.batch_number}</span></p>
+		<p class="flex h-full flex-row gap-2 self-start text-xs">PO <span>{item.expand?.stock_item_id.expand.stock_master_id.purchase_order}</span></p>
 	</div>
 	<div class="flex items-center gap-2 text-xs">
 		<Button disabled={isLoading} onclick={() => (isEditOpen = !isEditOpen)} size="icon" variant="outline"><Pencil class="h-4 w-4 text-lime-500" /></Button>
-		<Button disabled={isLoading} onclick={() => (isDeleteOpen = !isDeleteOpen)} size="icon" variant="outline"><Trash class="text-destructive h-4 w-4" /></Button>
+		<Button disabled={isLoading} onclick={() => (isDeleteOpen = !isDeleteOpen)} size="icon" variant="outline"><Trash class="h-4 w-4 text-destructive" /></Button>
 		{#if isLoading}
 			<LoaderCircle class="ml-2 h-4 w-4 animate-spin" />
 		{/if}
