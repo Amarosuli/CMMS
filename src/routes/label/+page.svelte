057f156e68b4e7028993104369be632657b0e678
amarosuli <vuvlnvw@gmail.com>
2026-06-08 19:07:53 +0700

    Update label page

--- src/routes/label/+page.svelte
+++ src/routes/label/+page.svelte
@@ -23,8 +23,8 @@
 	let confirmDialogOpen = $state(false);
 
 	const renderBarcode: Action<HTMLElement, string, Record<never, string>> = (node, data: string) => {
-		let identity: string = data;
-		jsBarcode(node, identity, {
+		let label: string = data;
+		jsBarcode(node, label, {
 			width: 1,
 			height: 25,
 			fontSize: 10
@@ -54,7 +54,7 @@
 
 			for (let m = 0; m < multiplier; m++) {
 				const key = `{${keyIndex}}bn`;
-				currentObject[key] = item.identity || '';
+				currentObject[key] = item.label || '';
 
 				if (keyIndex === 100) {
 					resultObjectArray.push(currentObject);
@@ -95,7 +95,7 @@
 
 			for (let m = 0; m < multiplier; m++) {
 				const key = `{${keyIndex}}bn`;
-				currentObject[key] = item.identity || '';
+				currentObject[key] = item.label || '';
 
 				if (keyIndex === 100) {
 					resultObjectArray.push(currentObject);
@@ -154,6 +154,7 @@
 	<ConfirmDialog title="Clear Label Cart" bind:open={confirmDialogOpen} onConfirm={clearLabelCart} />
 	<Button
 		variant="outline"
+		disabled={labelCarts.data.length ? false : true}
 		onclick={() => {
 			confirmDialogOpen = true;
 		}}><Eraser class="size-4" /> Clear Cart</Button>
@@ -174,12 +175,12 @@
 	<p class="mt-2 flex w-full items-center gap-2 text-yellow-600 lg:hidden"><Asterisk class="size-4" /> Web preview not available in mobile screen</p>
 
 	<div class="mt-4 hidden w-full flex-wrap lg:flex">
-		{#each labelCarts.data as label (label.identity)}
+		{#each labelCarts.data as label (label.label)}
 			<Tooltip.Root disableCloseOnTriggerClick>
 				<Tooltip.Trigger class="relative flex w-1/5 shrink-0 flex-col items-center justify-center border-[0.5px] py-2 text-[0.75rem]">
-					<Button class="absolute right-2 bottom-2 bg-red-200/50 hover:bg-red-200 dark:bg-red-400/50  dark:hover:bg-red-200" size="sm" variant="ghost" onclick={() => removeFromLabelCart(label.identity)}><X class="size-4 font-semibold text-red-500" /></Button>
+					<Button class="absolute right-2 bottom-2 bg-red-200/50 hover:bg-red-200 dark:bg-red-400/50  dark:hover:bg-red-200" size="sm" variant="ghost" onclick={() => removeFromLabelCart(label.label)}><X class="size-4 font-semibold text-red-500" /></Button>
 					<div class="flex items-center justify-center">
-						<canvas use:renderBarcode={label.identity}></canvas>
+						<canvas use:renderBarcode={label.label}></canvas>
 					</div>
 					<p class="text-primary">{label.expand?.stock_master_id.expand.material_master_id.part_number}</p>
 				</Tooltip.Trigger>
